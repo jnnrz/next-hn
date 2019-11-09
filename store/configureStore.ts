991ffe00235939417d4e185daa889d9036e5267d
Johann Perozo <johann.prz@gmail.com>
2019-11-09 18:35:11 -0400

    Improves configureStore

--- store/configureStore.ts
+++ store/configureStore.ts
@@ -1,10 +1,25 @@
-import { AppState, InitialAppState } from "./state";
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunkMiddleware from 'redux-thunk';
+import { combinedReducers } from './reducers';
+import { AppState, InitialAppState } from './state';
 
-const configureStoreComponent =
-  process.env.NODE_ENV === 'development'
-    ? require('./configureStore.dev')
-    : require('./configureStore.prod');
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: []
+};
+
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
 
 export const configureStore = (initialAppState: AppState = InitialAppState) => {
-  return configureStoreComponent.configureStore(initialAppState);
-}
+	return createStore(
+		persistedReducer,
+		initialAppState,
+		process.env.NODE_ENV === 'development'
+			? composeWithDevTools(applyMiddleware(thunkMiddleware))
+			: applyMiddleware(thunkMiddleware)
+	);
+};
