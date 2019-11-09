a6348c87d7150c5e2529d6a74a2d133197aa9307
Johann Perozo <johann.prz@gmail.com>
2019-11-09 18:41:01 -0400

    Adds redux to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import withRedux from 'next-redux-wrapper';
 import App from 'next/app';
 import { Router } from 'next/router';
 import nprogress from 'nprogress';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { compose } from 'redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { configureStore } from '../store/configureStore';
 import '../styles/index.css';
 
 Router.events.on('routeChangeStart', () => nprogress.start());
@@ -9,15 +15,28 @@ Router.events.on('routeChangeComplete', () => nprogress.done());
 Router.events.on('routeChangeError', () => nprogress.done());
 
 class MyApp extends App {
+	static async getInitialProps({ Component, ctx }) {
+		let pageProps = {};
 
-  render(): JSX.Element {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Component {...pageProps} />
-      </>
-    );
-  }
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+    }
+    
+		return { pageProps };
+	}
+
+	render() {
+		const { Component, pageProps, store }: any = this.props;
+		return (
+			<div>
+				<Provider store={store}>
+					<PersistGate persistor={persistStore(store)}>
+						<Component {...pageProps} />
+					</PersistGate>
+				</Provider>
+			</div>
+		);
+	}
 }
 
-export default MyApp;
+export default compose(withRedux(configureStore))(MyApp);
