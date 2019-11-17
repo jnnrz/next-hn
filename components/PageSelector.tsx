39ac9741211ccd0558933b6fb40de66aea6a00c2
Johann Perozo <johann.prz@gmail.com>
2019-11-16 20:22:46 -0400

    Adds PageSelector Component

---
+++ components/PageSelector.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+interface PageSelectorProps {
+	current: number;
+	maxPages: number;
+}
+
+const PageSelector = ({ current, maxPages }: PageSelectorProps) => {
+	const router = useRouter();
+
+	return (
+		<div className="mb-2 shadow bg-white p-2">
+			<div className="container mx-auto flex flex-row justify-center">
+				<button
+					className="selector-btn"
+					onClick={() => router.push(`/?page=${current - 1}`)}
+					disabled={current <= 1}
+				>
+					Previous
+				</button>
+				<span className="ml-4 mr-4">
+					{current} / {maxPages}
+				</span>
+				<button
+					className="selector-btn"
+					onClick={() => router.push(`/?page=${current + 1}`)}
+					disabled={current + 1 > maxPages}
+				>
+					Next
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default PageSelector;
